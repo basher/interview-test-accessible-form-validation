a9ea473e8df2cf63da83434f96296b19730974dd
basher <basher@madasafish.com>
2023-10-23 15:29:21 +0100

    scaffold repo using my ui boilerplate

---
+++ src/javascript/index.ts
@@ -0,0 +1,8 @@
+// Import Sass entry file.
+import '../../src/stylesheets/index.scss';
+
+// Import JavaScript modules.
+import FormValidate from './modules/form-validate';
+
+// UI module initialisation.
+FormValidate.start();
