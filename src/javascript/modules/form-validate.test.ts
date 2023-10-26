248c0e1d68e7a040b337fdb00eb75e7bc0b36b96
basher <basher@madasafish.com>
2023-10-26 16:12:49 +0100

    remove mock - form submit coverage now works

--- src/javascript/modules/form-validate.test.ts
+++ src/javascript/modules/form-validate.test.ts
@@ -1,7 +1,5 @@
 import FormValidate from './form-validate';
 
-jest.mock('./form-validate');
-
 describe('Validate form', () => {
     document.body.innerHTML = `
         <form data-module="form-validate">
@@ -14,11 +12,6 @@ describe('Validate form', () => {
     const form = document.querySelector(
         '[data-module="form-validate"]',
     ) as HTMLFormElement;
-
-    test('Class should have been called once', () => {
-        new FormValidate(form);
-        expect(FormValidate).toHaveBeenCalledTimes(1);
-    });
 
     test('Invalid input value on form submission', () => {
         new FormValidate(form);
