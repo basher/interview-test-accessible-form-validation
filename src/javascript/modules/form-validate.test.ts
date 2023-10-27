2798e3ffd868de77e9387dc52a96c72017f5c421
basher <basher@madasafish.com>
2023-10-27 12:12:52 +0100

    add test for valid input & error msg removal

--- src/javascript/modules/form-validate.test.ts
+++ src/javascript/modules/form-validate.test.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import FormValidate from './form-validate';
 
 describe('Validate form', () => {
@@ -7,6 +8,7 @@ describe('Validate form', () => {
                 <label for="name">Name</label>
                 <input type="text" id="name" name="name" value="" required />
             </div>
+            <button type="submit">Submit</button>
         </form>
     `;
 
@@ -14,6 +16,13 @@ describe('Validate form', () => {
         '[data-module="form-validate"]',
     ) as HTMLFormElement;
     const input = form.querySelector('#name') as HTMLInputElement;
+    const btnSubmit = form.querySelector(
+        '[type="submit"]',
+    ) as HTMLButtonElement;
+    const formFieldHasError = input.closest('.form__field');
+    const error = formFieldHasError?.querySelector(
+        '.form__error',
+    ) as HTMLSpanElement;
 
     FormValidate.start();
 
@@ -24,5 +33,21 @@ describe('Validate form', () => {
     test('Invalid input value on form submission', () => {
         form.submit();
         expect(input?.validity.valid).toBe(false);
+    });
+
+    // TODO - fix this test.
+    test('Blur event on submit button returns early', () => {
+        const evtBlur = jest.spyOn(FormValidate.prototype as any, 'handleBlur');
+        btnSubmit.blur();
+
+        expect(evtBlur).toHaveBeenCalled();
+    });
+
+    test('Blur event on valid input removes error', () => {
+        form.submit();
+        input.focus();
+        input.value = 'test';
+        input.blur();
+        expect(error).toBeFalsy();
     });
 });
